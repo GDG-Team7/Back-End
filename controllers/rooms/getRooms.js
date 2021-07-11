31325a5b95477fa1aee6ec54369cba505be33783
Heena Yun <hn.yun@heumlabs.io>
2021-07-11 10:17:10 +0900

    리스트 종료시간 및 연차 필터 추가

--- controllers/rooms/getRooms.js
+++ controllers/rooms/getRooms.js
@@ -1,16 +1,26 @@
 const { room, Sequelize } = require("../../models");
 module.exports = async (req, res) => {
-  const { member_limit, language, annual_min, annual_max } = req.query;
+  const { member_limit, language } = req.query;
+  let { annual_min, annual_max } = req.query;
 
   try {
     const findOptions = {};
 
+    findOptions["end_time"] = {};
     findOptions["end_time"][Sequelize.Op.gte] = Date.now();
     if (member_limit) {
       findOptions["member_limit"] = member_limit;
     }
     if (language) {
       findOptions["language"] = language;
+    }
+    if (annual_min || annual_max) {
+      if (!annual_min) annual_min = 0;
+      if (!annual_max) annual_max = 100;
+      findOptions[Sequelize.Op.and] = [
+        { annual_min: {[Sequelize.Op.gte]: annual_min} },
+        { annual_max: {[Sequelize.Op.lte]: annual_max} },
+      ];
     }
 
     const rooms = await room.findAll({
